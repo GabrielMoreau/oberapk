2b48ba606fcd2ec75a544055552175aa72cb6f0d
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-01-27 21:20:08 +0100

    Bug on chrome script

--- pakaj.d/chrome.sh
+++ pakaj.d/chrome.sh
@@ -14,6 +14,9 @@ function oberpakaj_chrome {
          google_chrome=$(zgrep ^Filename Packages.gz | grep '/google-chrome-stable/' | head -1 | awk '{print $2}')
 
          wget --timestamping "https://dl.google.com/linux/chrome/deb/${google_chrome}"
+         count=$(($(echo $(basename ${google_chrome}) | cut -f 4 -d '-' | cut -f 1 -d '_') + 1))
+         version=$(basename ${google_chrome} | cut -f 3 -d '-' | cut -f 2 -d '_')
+         package="google-chrome-stable_${version}-${count}_amd64.deb"
 
          # chrome
          tmp_folder=$(mktemp --directory /tmp/chrome-XXXXXX)
@@ -26,17 +29,20 @@ if [ -e "/etc/cron.daily/google-chrome" ]; then
   rm -f /etc/cron.daily/google-chrome
 fi
 END
+            size=$(du -ks ${tmp_folder} | cut -f 1)
+            sed -i -e "s/^Installed-Size: .*$/Installed-Size: ${size}/;
+                       s/^Version: .*$/Version: ${version}-${count}/;" control
             tar --owner root --group root -cJf control.tar.xz ./control ./postinst ./postrm ./prerm
             )
-         ar -r "$HOME/upload/chrome/$(basename ${google_chrome})" ${tmp_folder}/debian-binary ${tmp_folder}/control.tar.xz ${tmp_folder}/data.tar.xz
+         ar -r "${package}" ${tmp_folder}/debian-binary ${tmp_folder}/control.tar.xz ${tmp_folder}/data.tar.xz
          rm -rf ${tmp_folder}
 
-         if [ -e "$(basename ${google_chrome})" ]
+         if [ -e "${package}" ]
          then
             # Upload package
             for dist in ${distrib}
             do
-               ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/chrome/$(basename ${google_chrome}) )
+               ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/chrome/${package} )
             done
             ( cd ${REPREPRO} ; reprepro dumpreferences ) | grep '/google-chrome-stable'
          fi
