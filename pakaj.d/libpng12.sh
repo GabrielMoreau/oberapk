a0c2cefe83b88f12f261c50edccd69f29fb3124f
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-03-24 20:44:11 +0100

    Udate version for usrmerge

--- pakaj.d/libpng12.sh
+++ pakaj.d/libpng12.sh
@@ -1,6 +1,6 @@
 ## Date: 2022/03/24
 ## Pakaj: libpng12
-## Package: libpng12-0
+## Package: libpng12-1
 ## Author: Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
 ## See-Also: http://libpng.org/pub/png/libpng.html
 ## Description: PNG library - runtime version 12 needed by Ansys
@@ -13,18 +13,19 @@ function oberpakaj_libpng12 {
    mkdir -p "$HOME/upload/libpng12"
    cd "$HOME/upload/libpng12"
 
-   # patch >= 5 for usrmerge compatibility
-   version=1.2.54-5
-   package=libpng12-0_${version}_amd64.deb
+   # carrefull with usrmerge compatibility / change -0 to -1
+   version=1.2.54-1
+   package=libpng12-1_${version}_amd64.deb
 
-   if wget --timestamping http://ppa.launchpad.net/linuxuprising/libpng12/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1+1~ppa0~hirsute0_amd64.deb
+   if wget --timestamping http://ppa.launchpad.net/linuxuprising/libpng12/ubuntu/pool/main/libp/libpng/libpng12-1_1.2.54-1ubuntu1.1+1~ppa0~hirsute0_amd64.deb
    then
-      ar -x libpng12-0_1.2.54-1ubuntu1.1+1~ppa0~hirsute0_amd64.deb
+      ar -x libpng12-1_1.2.54-1ubuntu1.1+1~ppa0~hirsute0_amd64.deb
       tar xJf data.tar.xz
       tar xJf control.tar.xz
       rm -rf usr/share data.tar.xz control.tar.xz
 
-      sed -i -e "s/^Version: .*/Version: ${version}/;" control
+      sed -i -e "s/^Package: .*/Package: libpng12-1/;
+                 s/^Version: .*/Version: ${version}/;" control
       tar --owner root --group root -cJf control.tar.xz control
       tar --owner root --group root -cJf data.tar.xz usr
       ar -r $HOME/upload/libpng12/${package} debian-binary control.tar.* data.tar.*
@@ -44,5 +45,5 @@ function oberpakaj_libpng12 {
    fi
 
    # Clean old package - kept last 4 (put 4+1=5)
-   ls -t libpng12-0*.deb | tail -n +${keep} | xargs -r rm -f
+   ls -t libpng12-1*.deb | tail -n +${keep} | xargs -r rm -f
 }
