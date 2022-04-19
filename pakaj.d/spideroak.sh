f6b81b4ac6eaeaaa8781b77119a6b0b15a4ade0a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2022-04-19 11:14:22 +0200

    Remove spideroak source list

--- pakaj.d/spideroak.sh
+++ pakaj.d/spideroak.sh
@@ -17,16 +17,48 @@ function oberpakaj_spideroak {
    then
       if [ -e "Packages.gz" ]
       then
-         pkg_spideroak=$(zgrep ^Filename Packages.gz | grep '/spideroakone_' | head -1 | awk '{print $2}')
+         url=$(zgrep ^Filename Packages.gz | grep '/spideroakone_' | head -1 | awk '{print $2}')
+         pkg=$(basename ${url})
+         version=$(echo ${pkg} | cut -f 2 -d '_')'-1'
+         package="spideroakone_${version}_amd64.deb"
 
-         wget --timestamping "https://apt.spideroak.com/ubuntu-spideroak-hardy/${pkg_spideroak}"
+         wget --timestamping "https://apt.spideroak.com/ubuntu-spideroak-hardy/${url}"
 
-         if [ -e "$(basename ${pkg_spideroak})" ]
+         if [ -e "${pkg}" ]
+         then
+            tmp_folder=$(mktemp --directory /tmp/spideroak-XXXXXX)
+            (cd ${tmp_folder}
+               ar -x "$HOME/upload/spideroak/${pkg}"
+               tar -xzf control.tar.gz
+               tar -xJf data.tar.xz
+
+               # Remove apt source file
+               rm -rf etc/apt
+               grep -v spideroakone.list conffiles > conffiles.new
+               mv conffiles conffiles.old
+               mv conffiles.new conffiles
+
+               # Build new package archives
+               sed -i -e 's/^\(Version:.*\)$/\1-1/;' control
+               rm -f control.tar.gz data.tar.xz
+               tar -cJf data.tar.xz etc opt usr
+               tar -czf control.tar.gz conffiles control postinst postrm
+               )
+
+            # Create package (control before data)
+            ar -r ${package} ${tmp_folder}/debian-binary ${tmp_folder}/control.tar.gz ${tmp_folder}/data.tar.xz
+ 
+            # Clean
+            rm -rf ${tmp_folder}
+         fi
+
+         if [ -e "${package}" ]
          then
             # Upload package
             for dist in ${distrib}
             do
-               ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/spideroak/$(basename ${pkg_spideroak}) )
+               ( cd ${REPREPRO} ; reprepro dumpreferences ) 2>/dev/null | grep -q "^${dist}|.*/${package}" || \
+                  ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/spideroak/${package} )
             done
             ( cd ${REPREPRO} ; reprepro dumpreferences ) | grep '/spideroakone'
          fi
