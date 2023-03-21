db0d50322c6f93dead2f27f0e3bd36551d9ac7d4
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2023-03-21 16:09:47 +0100

    Add xflr5 package builder

---
+++ annex.d/make-soft-xflr5.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+# 2021/10/21 Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
+
+# See https://github.com/polmes/xflr5-ubuntu
+
+# Debian package
+sudo apt install qt5-qmake libgl1-mesa-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+
+tmp_folder=$(mktemp --directory /tmp/xflr5-XXXXXX)
+cd ${tmp_folder}
+
+git clone https://github.com/polmes/xflr5-ubuntu.git
+cd xflr5-ubuntu/xflr5/
+
+mkdir -p ${tmp_folder}/local/xflr5/usr
+
+PKG_NAME=xflr5
+PKG_VERSION=1
+CODE_VERSION=$(grep 'define .*_VERSION' xflr5v6/xflcore/gui_params.h | awk '{print $3}' | paste -s -d '.')
+
+qmake PREFIX=${tmp_folder}/local/xflr5/usr
+make
+make install
+
+cd ${tmp_folder}/local/xflr5
+find . -mtime -1
+
+sed -i -e 's/\/local//;' ./usr/share/xflr5/xflr5.desktop
+mkdir -p ./usr/share/applications
+mv ./usr/share/xflr5/xflr5.desktop ./usr/share/applications/
+
+# Data archive
+rm -f data.tar.gz
+tar --owner root --group root -czf data.tar.gz ./usr
+
+# Control file
+cat <<END > control
+Package: ${PKG_NAME}
+Version: ${CODE_VERSION}-${PKG_VERSION}
+Architecture: amd64
+Installed-Size: $(du -ks ./usr|cut -f 1)
+Maintainer:Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
+Depends: libc6, libgcc1, libgl1-mesa-glx | libgl1, libqt5opengl5, libqt5xml5, libqt5core5a, libqt5gui5, libstdc++6
+Section: science
+Priority: extra
+Homepage: http://www.xflr5.tech/xflr5.htm
+Description: airfoil, wings and plane analysis tool
+ XFLR5 is an analysis tool for airfoils, wings and planes operating
+ at low Reynolds Numbers.
+ It includes:
+ .
+  1. XFoil's Direct and Inverse analysis capabilities
+  2. Wing design analysis based on the Lifiting Line Theory and the
+    Vortex Lattice Method
+END
+
+# Control archive
+rm -f ${tmp_folder}/control.tar.gz
+tar --owner root --group root -czf control.tar.gz control
+
+# Format deb package
+echo 2.0 >  debian-binary
+
+# Create package (control before data)
+ar -r ${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_amd64.deb ./debian-binary ./control.tar.gz ./data.tar.gz
+
+
+# Specific post-install / Copy on reprepro server
+cat <<END
+scp ${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_amd64.deb repuser@repserver:bullseye/
+
+ssh repuser@repserver
+export REPDIR=/var/www/debian
+( cd \${REPDIR} ; reprepro includedeb bullseye   ~/bullseye/${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_amd64.deb )
+( cd \${REPDIR} ; reprepro dumpreferences ) | grep '/${PKG_NAME}'
+END
