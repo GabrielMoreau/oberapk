ce7bad6c4a1fc7c6b43ef436b424b00433c65364
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2024-06-13 16:01:06 +0200

    Big path bug !

--- pakaj.d/icaclient.sh
+++ pakaj.d/icaclient.sh
@@ -33,7 +33,7 @@ function oberpakaj_icaclient {
       for dist in ${distrib}
       do
          ( cd ${REPREPRO} ; reprepro dumpreferences )  2> /dev/null | grep -q "^${dist}|.*/${package}" || \
-            ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/zoom/${package} )
+            ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/icaclient/${package} )
          ( cd ${REPREPRO} ; reprepro dumpreferences ) | grep "^${dist}|.*/${pkg_basename}"
       done
    done
