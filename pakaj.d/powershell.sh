a20a964357a5a7cb6ba6ef342ae72796dfeeaae1
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-05-04 16:02:37 +0200

    Update powershell for each dist

--- pakaj.d/powershell.sh
+++ pakaj.d/powershell.sh
@@ -10,32 +10,38 @@ function oberpakaj_powershell {
    local keep=$1; shift
    local distrib=$*
 
-   mkdir -p "$HOME/upload/powershell"
-   cd "$HOME/upload/powershell"
-   PKG_VERSION=1
-   if wget --timestamping "https://packages.microsoft.com/repos/microsoft-debian-buster-prod/dists/buster/main/binary-amd64/Packages.gz"
-   then
-      if [ -e "Packages.gz" ]
+   for dist in ${distrib}
+   do
+      mkdir -p "$HOME/upload/powershell/${dist}"
+      cd "$HOME/upload/powershell/${dist}"
+      PKG_VERSION=1
+      if wget --timestamping "https://packages.microsoft.com/repos/microsoft-debian-${dist}-prod/dists/${dist}/main/binary-amd64/Packages.gz"
       then
-         url=$(zgrep ^Filename Packages.gz | grep '/powershell/' | head -1 | awk '{print $2}')
-         package=$(basename ${url})
-
-         wget --timestamping "https://packages.microsoft.com/repos/microsoft-debian-buster-prod/${url}"
-
-         if [ -s "${package}" ]
+         if [ -e "Packages.gz" ]
          then
-            # Upload package
-            for dist in ${distrib}
-            do
-               ( cd ${REPREPRO} ; reprepro dumpreferences ) 2> /dev/null | grep -q "^${dist}|.*/${package}" || \
-                  ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/powershell/${package} )
-               ( cd ${REPREPRO} ; reprepro dumpreferences ) 2> /dev/null | grep "^${dist}|.*/${package}"
-            done
+            url=$(zgrep ^Filename Packages.gz | grep '/powershell/' | head -1 | awk '{print $2}')
+            package=$(basename ${url})
+
+            wget --timestamping "https://packages.microsoft.com/repos/microsoft-debian-${dist}-prod/${url}"
+
+            if [ -s "${package}" ]
+            then
+               # Upload package
+               for dist in ${distrib}
+               do
+                  ( cd ${REPREPRO} ; reprepro dumpreferences ) 2> /dev/null | grep -q "^${dist}|.*/${package}" || \
+                     ( cd ${REPREPRO} ; reprepro includedeb ${dist} $HOME/upload/powershell/${dist}/${package} )
+                  ( cd ${REPREPRO} ; reprepro dumpreferences ) 2> /dev/null | grep "^${dist}|.*/${package}"
+               done
+            fi
          fi
       fi
-   fi
-   
-   # Clean old package - kept last 4 (put 4+1=5)
-   cd "$HOME/upload/powershell"
-   ls -t powershell_*.deb | tail -n +$((${keep} + 1)) | xargs -r rm -f
+   done
+
+   for dist in ${distrib}
+   do
+      # Clean old package - kept last 4 (put 4+1=5)
+      cd "$HOME/upload/powershell"
+      ls -t powershell_*.deb | tail -n +$((${keep} + 1)) | xargs -r rm -f
+   done
    }
