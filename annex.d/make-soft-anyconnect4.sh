eb8242184a98641c136cc13a9a92822e8b8dc286
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-09-05 10:26:02 +0200

    Better code with shellcheck

--- annex.d/make-soft-anyconnect4.sh
+++ annex.d/make-soft-anyconnect4.sh
@@ -24,51 +24,51 @@ pkg_name=anyconnect
 package=${pkg_name}_${pkg_version}_amd64.deb
 
 tmp_folder=$(mktemp --directory /tmp/anyconnect-XXXXXX)
-(cd ${tmp_folder}
+(cd "${tmp_folder}" || exit 1
 
-# Data archive
-mkdir opt
-mkdir -p etc/systemd/system
-rsync -a --exclude 'temp' /opt/cisco  ./opt/
-rsync -a /opt/.cisco ./opt/
-rsync -a /etc/systemd/system/vpnagentd.service ./etc/systemd/system/
+   # Data archive
+   mkdir opt
+   mkdir -p etc/systemd/system
+   rsync -a --exclude 'temp' /opt/cisco  ./opt/
+   rsync -a /opt/.cisco ./opt/
+   rsync -a /etc/systemd/system/vpnagentd.service ./etc/systemd/system/
 
-rm -f ./opt/cisco/anyconnect/anyconnect-*.log
-rm -f ./opt/cisco/anyconnect/bin/*_uninstall.sh
+   rm -f ./opt/cisco/anyconnect/anyconnect-*.log
+   rm -f ./opt/cisco/anyconnect/bin/*_uninstall.sh
 
-for file in /usr/share/applications/com.cisco.anyconnect.gui.desktop \
-   /usr/share/icons/hicolor/128x128/apps/cisco-anyconnect.png \
-   /usr/share/icons/hicolor/256x256/apps/cisco-anyconnect.png \
-   /usr/share/icons/hicolor/48x48/apps/cisco-anyconnect.png \
-   /usr/share/icons/hicolor/512x512/apps/cisco-anyconnect.png \
-   /usr/share/icons/hicolor/64x64/apps/cisco-anyconnect.png \
-   /usr/share/icons/hicolor/96x96/apps/cisco-anyconnect.png \
-   /usr/share/desktop-directories/cisco-anyconnect.directory
-do
-   mkdir -p .$(dirname ${file})
-   cp -p ${file} .${file}
-done
-
-find . -perm /0100 -a -name '*.png'  -a -type f -exec chmod a-x {} \+
-find . -perm /0100 -a -name '*.so'   -a -type f -exec chmod a-x {} \+
-find . -perm /0100 -a -name '*.so.*' -a -type f -exec chmod a-x {} \+
-find . -type f -exec chmod u+rw,go+r {} \+
-find . -type d -exec chmod u+rwx,go+rx {} \+
-
-mkdir -p usr/bin
-(cd usr/bin; ln -s ../../opt/cisco/anyconnect/bin/vpnui vpnui)
-
-tar --preserve-permissions --owner root --group root -cJf data.tar.xz ./etc ./usr ./opt
-
-depends=$(for lib in $(ldd /opt/cisco/anyconnect/bin/vpnui | grep -v /opt/ | grep '=>' | awk '{print $3}')
+   for file in /usr/share/applications/com.cisco.anyconnect.gui.desktop \
+      /usr/share/icons/hicolor/128x128/apps/cisco-anyconnect.png \
+      /usr/share/icons/hicolor/256x256/apps/cisco-anyconnect.png \
+      /usr/share/icons/hicolor/48x48/apps/cisco-anyconnect.png \
+      /usr/share/icons/hicolor/512x512/apps/cisco-anyconnect.png \
+      /usr/share/icons/hicolor/64x64/apps/cisco-anyconnect.png \
+      /usr/share/icons/hicolor/96x96/apps/cisco-anyconnect.png \
+      /usr/share/desktop-directories/cisco-anyconnect.directory
    do
-      echo "${lib}" | egrep -q '/(libsystemd.so|libstdc++.so|libgobject-2.0.so|libglib-2.0.so|libXext.so)' && continue
-      # echo =========== $lib
-      apt-file search $lib
-   done | cut -f 1 -d ':' | sort -u | paste -sd ','
-   )
+      mkdir -p ".$(dirname "${file}")"
+      cp -p "${file}" ".${file}"
+   done
 
-# Control archive
+   find . -perm /0100 -a -name '*.png'  -a -type f -exec chmod a-x {} \+
+   find . -perm /0100 -a -name '*.so'   -a -type f -exec chmod a-x {} \+
+   find . -perm /0100 -a -name '*.so.*' -a -type f -exec chmod a-x {} \+
+   find . -type f -exec chmod u+rw,go+r {} \+
+   find . -type d -exec chmod u+rwx,go+rx {} \+
+
+   mkdir -p usr/bin
+   (cd usr/bin || exit; ln -s ../../opt/cisco/anyconnect/bin/vpnui vpnui)
+
+   tar --preserve-permissions --owner root --group root -cJf data.tar.xz ./etc ./usr ./opt
+
+   depends=$(for lib in $(ldd /opt/cisco/anyconnect/bin/vpnui | grep -v /opt/ | grep '=>' | awk '{print $3}')
+      do
+        echo "${lib}" | grep -Eq '/(libsystemd.so|libstdc++.so|libgobject-2.0.so|libglib-2.0.so|libXext.so)' && continue
+        # echo =========== $lib
+        apt-file search "$lib"
+      done | cut -f 1 -d ':' | sort -u | paste -sd ','
+      )
+
+   # Control archive
 cat <<'END' > postinst
 #!/bin/sh
 # Updating GTK icon cache
@@ -82,18 +82,18 @@ systemctl start vpnagentd
 #ln -s /opt/cisco/anyconnect/bin/vpnui /usr/bin/vpnui
 END
 
-cat <<'END' > prerm
+   cat <<'END' > prerm
 #!/bin/sh
 #rm -f /usr/bin/vpnui
 systemctl status vpnagentd | grep -q 'Active: active' && systemctl stop vpnagentd
 systemctl disable vpnagentd
 END
 
-chmod a+rx ./postinst ./prerm
+   chmod a+rx ./postinst ./prerm
 
-size=$(du -ks ${tmp_folder} | cut -f 1)
+   size=$(du -ks "${tmp_folder}" | cut -f 1)
 
-cat <<END > control
+   cat <<END > control
 Package: ${pkg_name}
 Version: ${pkg_version}
 Section: utils
@@ -110,24 +110,27 @@ Description: Cisco AnyConnect Secure Mobility Client
 Homepage: https://www.cisco.com/c/en/us/support/security/secure-client-5/model.html
 END
 
-tar --owner root --group root -czf control.tar.gz ./control ./postinst ./prerm
+   tar --owner root --group root -czf control.tar.gz ./control ./postinst ./prerm
 )
 
 # Format deb package
-echo 2.0 > ${tmp_folder}/debian-binary
+echo 2.0 > "${tmp_folder}/debian-binary"
 
 # Create package (control before data)
-ar -r ${package} ${tmp_folder}/debian-binary ${tmp_folder}/control.tar.gz ${tmp_folder}/data.tar.xz
+ar -r "${package}" "${tmp_folder}/debian-binary" "${tmp_folder}/control.tar.gz" "${tmp_folder}/data.tar.xz"
 
 # Clean
 # rm -rf ${tmp_folder}
 
-#
-echo '# Copy the package on the reprepo server and then'
-echo 'export REPO=/var/www/debian/'
-echo '(cd ${REPO} ; reprepro includedeb bullseye $HOME/bullseye/'"${package})"
-echo '(cd ${REPO} ; reprepro dumpreferences ) | grep anyconnect'
-echo '# (cd ${REPO} ; reprepro remove bullseye anyconnect )'
-echo ''
-echo '# Clean the temporary build folder'
-echo "rm -rf ${tmp_folder}"
+# Final reprepo bonus
+cat <<'END'
+# Copy the package on the reprepo server and then
+export REPO=/var/www/debian/
+export DIST=bookworm
+(cd "${REPO}" ; reprepro includedeb "${DIST}" "$HOME/${DIST}/${package})"
+(cd "${REPO}" ; reprepro dumpreferences ) | grep anyconnect
+# (cd "${REPO}" ; reprepro remove "${DIST}" anyconnect )
+
+# Clean the temporary build folder
+rm -rf "${tmp_folder}"
+END
