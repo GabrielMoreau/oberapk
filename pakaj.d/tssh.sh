3deb7d55ac910ca2085a1070f171304fe95a150b
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2025-10-13 15:38:51 +0200

    More robust bash code

--- pakaj.d/tssh.sh
+++ pakaj.d/tssh.sh
@@ -13,13 +13,13 @@ function oberpakaj_tssh {
 
    if [ ! -d "${HOME}/upload/tssh" ]
    then
-      cd "${HOME}/upload/"
+      cd "${HOME}/upload/" || return
       git clone https://gricad-gitlab.univ-grenoble-alpes.fr/legi/soft/trokata/tssh.git
    fi
 
    if [ -d "${HOME}/upload/tssh/.git" ]
    then
-      cd "${HOME}/upload/tssh"
+      cd "${HOME}/upload/tssh" || return
       git pull
 
       PKG_NAME=$(grep '^PKG_NAME=' make-package-debian | cut -f 2 -d "=")
@@ -27,14 +27,18 @@ function oberpakaj_tssh {
       PKG_VERSION=$(grep '^PKG_VERSION=' make-package-debian | cut -f 2 -d "=")
       package=${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_all.deb
 
-      if [ ! -e "${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_all.deb" ]
+      if [ ! -s "${package}" ]
       then
          make
          make pkg
+      fi
 
+      if [ -s "${package}" ] && file "${package}" | grep -q 'Debian binary package'
+      then
          for dist in ${distrib}
          do
-           ( cd "${REPREPRO}" || return ; reprepro includedeb "${dist}" "$HOME/upload/${PKG_NAME}/${package}" )
+            ( cd "${REPREPRO}" || return ; reprepro dumpreferences ) 2> /dev/null | grep -q "^${dist}|.*/${package}" || \
+               ( cd "${REPREPRO}" || return ; reprepro includedeb "${dist}" "$HOME/upload/${PKG_NAME}/${package}" )
          done
          ( cd "${REPREPRO}" || return ; reprepro dumpreferences ) | grep -i "/${PKG_NAME}"
       fi
@@ -43,7 +47,7 @@ function oberpakaj_tssh {
    # Clean old package - keep last 4 (put 4+1=5)
    if [ -d "${HOME}/upload/tssh" ]
    then
-      cd "${HOME}/upload/tssh"
+      cd "${HOME}/upload/tssh" || return
       ls -1t -- tssh_*.deb 2> /dev/null | tail -n +$((keep+1)) | xargs -r rm -f --
    fi
    }
