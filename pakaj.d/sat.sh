79e063b93db655307288c4326a3ec3fa6b6d9031
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-07-28 14:16:21 +0200

    Add sat package

---
+++ pakaj.d/sat.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+## Date: 2026/07/28
+## Pakaj: sat
+## Author: Gabriel Moreau <Gabriel.Moreau@univ-grenoble-alpes.fr>
+## See-Also: https://gricad-gitlab.univ-grenoble-alpes.fr/legi/soft/trokata/sat
+## Description: Simple At like interface with systemd persistence
+## Binaries: ls tail xargs reprepro git grep cut make pod2man
+
+function oberpakaj_sat {
+   local keep=$1; shift
+   local distrib=$*
+
+   pakajname=$(echo "${FUNCNAME[0]}" | sed -e 's/^oberpakaj_//; s/_/-/g;')
+   if [ ! -d "${HOME}/upload/${pakajname}" ]
+   then
+      cd "${HOME}/upload/" || return
+      git clone https://gricad-gitlab.univ-grenoble-alpes.fr/legi/soft/trokata/${pakajname}.git
+   fi
+
+   if [ -d "${HOME}/upload/${pakajname}/.git" ]
+   then
+      cd "${HOME}/upload/${pakajname}" || return
+      git pull
+
+      PKG_NAME=$(grep '^PKG_NAME=' make-package-debian | cut -f 2 -d "=")
+      CODE_VERSION=$(./get-version)
+      PKG_VERSION=$(grep '^PKG_VERSION=' make-package-debian | cut -f 2 -d "=")
+      package=${PKG_NAME}_${CODE_VERSION}-${PKG_VERSION}_all.deb
+
+      if [ ! -s "${package}" ]
+      then
+         ./make-package-debian
+      fi
+
+      if [ -s "${package}" ] && file "${package}" | grep -q 'Debian binary package'
+      then
+         for dist in ${distrib}
+         do
+            ( cd "${REPREPRO}" || return ; reprepro dumpreferences ) 2> /dev/null | grep -q "^${dist}|.*/${package}" || \
+               ( cd "${REPREPRO}" || return ; reprepro includedeb "${dist}" "$HOME/upload/${pakajname}/${package}" )
+         done
+         ( cd "${REPREPRO}" || return ; reprepro dumpreferences ) | grep -i "/${PKG_NAME}"
+      fi
+   fi
+
+   # Clean old package - keep last 4 (put 4+1=5)
+   if [ -d "${HOME}/upload/${pakajname}" ]
+   then
+      cd "${HOME}/upload/${pakajname}" || return
+      ls -1t -- "${pakajname}"_*.deb 2> /dev/null | tail -n +$((keep+1)) | xargs -r rm -f --
+   fi
+   }
